6e5de49f386f9d6342e4fc2022f376d4b0c2d428
emonthecode-wizard <amianik5566@gmail.com>
2023-08-29 11:27:45 +0600

    Version 2

--- phone.js
+++ phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json();
     // console.log(data);
     const phones = data.data;
@@ -10,8 +10,21 @@ const loadPhone = async () => {
 const displayPhones = phones => {
     //step 1: get container div id 
     const phoneContainer = document.getElementById('phone-container');
+    // Clear Previous Data
+    phoneContainer.textContent = '';
+
+    // Display Show All Button if there are more than 12 phone
+
+    const showAllContainer = document.getElementById('show-all-container')
+    if (phones.length > 12) {
+        showAllContainer.classList.remove('hidden')
+    }
+    else {
+        showAllContainer.classList.add('hidden')
+    }
+    phones = phones.slice(0, 12);
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         // step 2 create a div
         const phoneCard = document.createElement('div');
         // step 3 set innerHtml
@@ -30,7 +43,33 @@ const displayPhones = phones => {
         `;
         // step 4 appendChild div to a container
         phoneContainer.appendChild(phoneCard);
-    })
+    });
+    // Hide Loading Spinner:
+
+    toggleLoadingSpinner(false);
 }
 
-loadPhone()
+
+// Handle Search Button
+
+const handleSearch = () => {
+    toggleLoadingSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhone(searchText);
+}
+
+
+const toggleLoadingSpinner = (isLoading) => {
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if (isLoading) {
+        loadingSpinner.classList.remove('hidden')
+    }
+    else {
+        loadingSpinner.classList.add('hidden')
+    }
+
+}
+
+
+// loadPhone()
